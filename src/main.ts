cc41d6d4ecee639fc9355cc2dab16fc05a40dce9
Yim Klok <yimklok.kh@gmail.com>
2024-03-05 15:28:57 +0700

    change custom log to Logger of nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 // =========================================================================>> Core Library
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 // =========================================================================>> Third Party Library
 import * as expressHandlebars from 'express-handlebars';
-import { join } from 'path'; 
+import { join } from 'path';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 
@@ -47,16 +47,8 @@ const bootstrap = async () => {
     // Port app running
     const PORT = process.env.PORT || 1000;
     app.listen(PORT, () => {
-        // Manually format the date to "MM/DD/YYYY"
-        const now = new Date();
-        const formattedDate = `${(now.getMonth() + 1).toString().padStart(2, '0')}/${now.getDate().toString().padStart(2, '0')}/${now.getFullYear()}`;
-        // Determine AM or PM
-        const amPm = now.getHours() >= 12 ? 'PM' : 'AM';
-        // Convert to 12-hour format
-        const hours12 = now.getHours() % 12 || 12; // Converts "0" hours to "12" for 12 AM
-        // Format the time with leading zeros and include AM/PM
-        const formattedTime = `${hours12.toString()}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')} ${amPm}`;
-        console.log(`\x1b[32m[Nest] ${process.pid}  - \x1b[37m${formattedDate}, ${formattedTime}     \x1b[32mLOG \x1b[33m[NestApplication] \x1b[32mNest application running on host: \x1b[34mhttp://localhost:${PORT}\x1b[37m`);
+        const logger = new Logger('NestApplication');
+        logger.log(`\x1b[32mNest application running on host: \x1b[34mhttp://localhost:${PORT}\x1b[37m`)
     });
 }
 bootstrap();
