68a7bb227c732390c9a2057a45d4eaf0e07ffb94
kiry <kongchankiry2002@gmail.com>
2024-05-01 19:50:15 +0700

    connect api to file and seeding data for photo

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -14,25 +14,19 @@ interface File {
 export class FileService {
 
     private fileBaseUrl  = process.env.FILE_BASE_URL;
-    private fileKey      = process.env.FILE_KEY;
-    private fileUsername = process.env.FILE_USERNAME;
-    private filePassword = process.env.FILE_PASSWORD;
 
     public uploadBase64Image = async (folder: string, base64: string) => {
         const result: { file?: File, error?: string } = {};
         try {
             const body = {
-                key: this.fileKey,
                 folder: folder,
                 image: base64
             }
-            const response = await axios.post(this.fileBaseUrl + '/api/file/upload-base64', body, {
-                headers: {
-                    Authorization: `Basic ${btoa(`${this.fileUsername}:${this.filePassword}`)}`
-                }
-            });
+            const response = await axios.post(this.fileBaseUrl + '/service/file/upload/base64', body);
             result.file = response.data.data;
+            
         } catch (error) {
+            console.log(error);
             result.error = error?.response?.data?.message || 'Something when wrong. Failed call to saving the file';
         }
         return result
@@ -42,15 +36,9 @@ export class FileService {
         const result: { file?: File, error?: string } = {};
         try {
             const formData = new FormData();
-            formData.append('key', this.fileKey);
             formData.append('folder', folder);
             formData.append('file', file.buffer, file.originalname);
-            const response = await axios.post(this.fileBaseUrl + '/api/file/upload-single', formData, {
-                headers: {
-                    ...formData.getHeaders(),
-                    Authorization: `Basic ${btoa(`${this.fileUsername}:${this.filePassword}`)}`
-                }
-            });
+            const response = await axios.post(this.fileBaseUrl + '/service/file/upload', formData);
             result.file = response.data.data;
         } catch (error) {
             result.error = error?.response?.data?.message || 'Something when wrong';
