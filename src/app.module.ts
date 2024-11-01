94acba83a3479655f44807d4aefefb375c3aac85
unknown <your_email@example.com>
2024-11-01 05:29:03 -0700

    structure file project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegramService } from './services/telegram.service';
+import { FileService } from './services/file.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TelegramService, FileService],
 })
 export class AppModule {}
